3c542c0846311b60c804df3937e8a33ccbabda47
Etter <dominic.etter@students.fhnw.ch>
2019-10-11 09:12:38 +0200

    Ergebnismethode erweitert auf n-Spieler

--- src/poker/version_graphics/model/PokerGameModel.java
+++ src/poker/version_graphics/model/PokerGameModel.java
@@ -7,7 +7,8 @@ import poker.version_graphics.PokerGame;
 public class PokerGameModel {
 	private final ArrayList<Player> players = new ArrayList<>();
 	private DeckOfCards deck;
-	private String besterSpieler;
+	private String besterSpieler, unentSpieler="";
+	private int gleichstand=0, gewinner=0, tempunentschieden=0; 
 	
 	public PokerGameModel() {
 		for (int i = 0; i < PokerGame.NUM_PLAYERS; i++) {
@@ -25,24 +26,118 @@ public class PokerGameModel {
 		return deck;
 	}
 
-	//Gibt den Gewinner des Spiels zurück
+	//Gibt den Gewinner des Spiels zurück 
+	
+	//Problem wenn player1 gewinnen sollte!!!!!!!!!!!
 	public String returnWinner() {
+		this.unentSpieler=""; //String zurücksetzen
 		
-		for (int i = 0; i < (PokerGame.NUM_PLAYERS-1); i++) {
-			int j = getPlayer(i).compareTo(getPlayer(i+1));
+			// 2 Spielerfall
+			/*int j = getPlayer(0).compareTo(getPlayer(1));
 			
 			if(j>0) {
-				this.besterSpieler = "Der Gewinner ist " +getPlayer(i).getPlayerName();
+				this.besterSpieler = "Der Gewinner ist " +getPlayer(0).getPlayerName();
 			}
 			if (j<0) {
-				this.besterSpieler = "Der Gewinner ist " +getPlayer(i+1).getPlayerName();
+				this.besterSpieler = "Der Gewinner ist " +getPlayer(1).getPlayerName();
 			}
 			if (j==0) {
-				this.besterSpieler = "Gleichstand zwischen " + getPlayer(i).getPlayerName() + " und " + getPlayer(i+1).getPlayerName();
-			}			
-		}
+				this.besterSpieler = "Gleichstand zwischen " + getPlayer(0).getPlayerName() + " und " + getPlayer(1).getPlayerName();
+			}
+			*/
 		
+			//Erweiterung mehr als 2 Spieler
+			
+			for (int i = (PokerGame.NUM_PLAYERS-1); i >= 0; i--) {
+				int tempgewinner=1000;// zeigt ob der i-Spieler der Gewinner ist
+				if (i==(PokerGame.NUM_PLAYERS-1)) {
+					this.tempunentschieden=0; // in der aller ersten Runde setzen, damit Vorrunde keinen Einfluss
+					System.out.println(this.tempunentschieden);
+				}
+				for (int j = 0; j < (PokerGame.NUM_PLAYERS); j++ ) {
+					
+					if((i>0 && j==0)||(i==0 & j==1)  ) { //im ersten Durchlauf eines Spieler muss es möglich sein in die Gewinnerschleife zu kommen
+					tempgewinner=i;
+					}
+					
+					if(i==j) {
+					}else{
+						System.out.println("Runde "+i + " " + j);
+						System.out.println("Rundetemp "+tempgewinner);
+						
+						int m = getPlayer(i).compareTo(getPlayer(j));
+							if(m>0 && tempgewinner == i) {
+								tempgewinner = i;
+								tempunentschieden = 0;
+								System.out.println("temp"+tempgewinner + "unent" + tempunentschieden);
+								}
+							
+							/*
+							 * 
+							 * Hier hat es einen Bug, es bleibt immer ein Fall übrig, der nicht stimmt!
+							 * Nur bei >2 Spielern ein Problem
+							 */
+							
+						
+							if (m<0) {
+								tempgewinner = 1000;
+								if(i==0) {//Stellt sicher, dass höhere vorhergehende Unentschieden nicht auf 0 gesetzt
+									if(j==(PokerGame.NUM_PLAYERS-1)) {
+										tempunentschieden = 0;
+									}
+								}else {
+									tempunentschieden = 0; 
+								}
+								
+								
+								System.out.println("looser"+tempgewinner);
+								}
+							
+							
+							/*
+							 * 
+							 * 
+							 */
+							
+							if (m==0) {
+								if (tempgewinner==i) {
+									tempunentschieden=1;
+									tempgewinner=500;
+									this.unentSpieler= this.unentSpieler.concat(i+" ");
+									}
+								if(tempgewinner == 500) {
+									tempunentschieden=1;
+								}
+								if (tempgewinner == 1000) {
+									tempunentschieden=0;
+								}
+
+							}
+					}
+					
+				}
+				
+				System.out.println("Unentschieden"+this.tempunentschieden);
+				if(tempgewinner == i) {
+					this.gewinner = tempgewinner;
+					System.out.println("Gewinner"+this.gewinner);
+					}
+			
+			}
+			
+			if (this.gewinner >=0 && this.gewinner< PokerGame.NUM_PLAYERS) {
+				if(tempunentschieden == 1) {
+					this.besterSpieler = "Gleichstand zwischen den Spielern: " + this.unentSpieler ;
+				}else {
+					this.besterSpieler = "Der Gewinner ist " +getPlayer(this.gewinner).getPlayerName();
+				}	
+			}else {
+				this.besterSpieler = "Gleichstand zwischen den Spielern: " + this.unentSpieler ;
+				}
+			
 		return besterSpieler;
-		
 	}
-}
+}	
+				
+
+
